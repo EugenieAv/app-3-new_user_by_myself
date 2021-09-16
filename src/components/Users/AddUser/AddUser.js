da000d58911bfaba6d0fd9234cd43b5f928295ba
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-09-16 15:16:40 +0200

    uncontrolled component with refs method

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, Fragment, useRef} from 'react';
 import Button from '../../UI/Button/Button';
 import Card from '../../UI/Card/Card';
 import ErrorModal from '../../UI/ErrorModal/ErrorModal';
@@ -9,23 +9,29 @@ import styles from './AddUser.module.css';
 
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    // const [enteredUsername, setEnteredUsername] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
 
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.currentTarget.value);
-    };
+    // const usernameChangeHandler = (event) => {
+    //     setEnteredUsername(event.currentTarget.value);
+    // };
     
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.currentTarget.value);
-    };
+    // const ageChangeHandler = (event) => {
+    //     setEnteredAge(event.currentTarget.value);
+    // };
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        const nameInput = (nameInputRef.current.value);
+        const ageInput = (ageInputRef.current.value);
+
+        if(nameInput.trim().length === 0 || ageInput.trim().length === 0){
             setError({
                 title: 'Invalid Input',
                 message: 'Please enter a valid name and age (non-empty values).'
@@ -35,17 +41,19 @@ const AddUser = (props) => {
         
         // on force la convertion de enteredAge en number avec le + car il est initialisé en tant que String
         // et on le compaer à 1. Ca devrait marcher mais pour être sûr , on compare 2 éléments de même nature
-        if (+enteredAge < 1){
+        if (+ageInput < 1){
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid  age (>0).'
             });
             return;
         }
-
-        props.addUserToList(enteredUsername, enteredAge);
-        setEnteredUsername('');
-        setEnteredAge(''); 
+        props.addUserToList(nameInput, ageInput);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+        // props.addUserToList(enteredUsername, enteredAge);
+        // setEnteredUsername('');
+        // setEnteredAge(''); 
     };
 
     const errorHandler = () => {
@@ -62,9 +70,21 @@ const AddUser = (props) => {
             <Card className={styles.input}> 
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username" >Username</label>
-                    <input id="username" type='text' onChange={usernameChangeHandler} value={enteredUsername}></input>
+                    <input 
+                        id="username" 
+                        type='text' 
+                        // onChange={usernameChangeHandler} 
+                        // value={enteredUsername}
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age (years)</label>
-                    <input type="number" id="age" onChange={ageChangeHandler} value={enteredAge} ></input>
+                    <input 
+                        type="number" 
+                        id="age" 
+                        // onChange={ageChangeHandler} 
+                        // value={enteredAge}
+                        ref={ageInputRef}
+                    />
                     <Button type="submit"> Add user </Button>
                 </form>
             </Card>
